1e0d5ad58e81caba1ae610dd3831f0fa083c6d07
brandon-github <brandonmachado159@gmail.com>
2022-08-27 11:15:13 -0500

    implementacion de controlador para el acceso a los datos de los juegos

---
+++ controllers/GameController.php
@@ -0,0 +1,32 @@
+<?php
+
+require './config/Connection.php';
+
+class GameController 
+{
+    public static function getAll()
+    {
+
+    }
+
+    public static function getAllByUser($id)
+    {
+
+    }
+
+    public static function create($data)
+    {
+
+    }
+
+    public static function update($id, $data)
+    {
+
+    }
+
+    public static function delete($id)
+    {
+
+    }
+
+}
